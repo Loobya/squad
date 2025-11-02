b31ba98ca93af0e07c5bfeacf6ead770a0794eb2
Loobya <39651614+Loobya@users.noreply.github.com>
2025-11-02 14:06:58 +0100

    Delete project_root directory

--- project_root/java_app/run_editor.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-set "JAVA_FX_PATH=C:\Users\Asus\Desktop\javafx\javafx-sdk-25\lib"
-
-echo Starting Scenario Editor...
-java -jar --module-path "%JAVA_FX_PATH%" --add-modules javafx.controls,javafx.graphics build\scenario_editor.jar
-
-pause
